a7d095860496bc805305cf55f2890d91b4344e6f
Yaroshevich Vlad <yaroshevich00@mail.ru>
2021-07-19 12:06:02 +0300

    401 fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,15 +4,13 @@ const AuthentificationError = require('../errors/authentification-err');
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthentificationError('Необходима авторизация');
   }
-
-  const token = authorization.replace('Bearer', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, 'jwt_secret_key');
+    payload = jwt.verify(authorization, 'jwt_secret_key');
   } catch (err) {
     next(new AuthentificationError('Необходима авторизация'));
   }
