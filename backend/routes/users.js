db90bf2af3ab9d2c90f89eec7a21a7b334e774d9
Yaroshevich Vlad <yaroshevich00@mail.ru>
2021-07-20 16:25:07 +0300

    fix2

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -17,8 +17,8 @@ usersRouter.get('/:userId', celebrate({
 
 usersRouter.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 
